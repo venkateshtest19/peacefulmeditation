d317efb736b7b8ffcf20a4f6e68f54333c632517
venkateshtest19 <venkatesh.mahe.edu@gmail.com>
2026-09-17 07:10:13 +0530

    popup-config.js

--- popup-config.js
+++ popup-config.js
@@ -5,7 +5,7 @@
 */
 
 const popupConfig = {
-  isEnabled: true,          // SET TO false TO DISABLE ALL POPUPS
+  isEnabled: false,          // SET TO true / false TO DISABLE ALL POPUPS
   activePopupId: 2,         // CHOOSE: 1 for apple, 2 for Meditation
 
   popups: [
